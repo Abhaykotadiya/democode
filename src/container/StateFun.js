7996116beace212f1322587a678fc402a9f64c72
Abhaykotadiya <abhaykotadiya22@gmail.com>
2022-04-05 08:37:09 +0530

    statefun

--- src/container/StateFun.js
+++ src/container/StateFun.js
@@ -1,24 +1,30 @@
-import React, { useState} from 'react'
+import React, { useState } from "react";
 
 export default function StateFun() {
+  const [id, setId] = useState(111);
+  const [name, setName] = useState("raj");
+  const [city, setCity] = useState("surat");
 
-    const [id,setId] = useState(111);
-    const [name,setName] = useState('raj');
-    
-    const change = () => {
-        setId(112);
-    }
+  const change = () => {
+    setId(112);
+  };
+  const citychange = () => {
+    setCity("Ahmedabad");
+  };
   return (
     <>
-    <div>
-    {id}
-    <button onClick={ () => change()}>change id</button>
-    </div>
-    <div>
-    {name}
-    <button onClick={ () => setName('Abhay')}>change Name</button>
-    </div>
+      <div>
+        {id}
+        <button onClick={() => change()}>change id</button>
+      </div>
+      <div>
+        {name}
+        <button onClick={() => setName("Abhay")}>change Name</button>
+      </div>
+      <div>
+        {city}
+        <button onClick={() => citychange()}>change Name</button>
+      </div>
     </>
-    
-  )
+  );
 }
