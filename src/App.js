493d148204e7740b135b3898bf5d0744fb8e2a0c
Abhaykotadiya <abhaykoatdiya22@gmail.com>
2022-05-03 18:23:04 +0530

    impliment

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
-import { StrictMode } from 'react';
-import Smd from "./container/Smd"
-import State from './container/State';
-import Country from './container/Country';
-import StateFun from './container/StateFun';
-import City from './container/City';
-import Counter from './container/Counter';
+import Header from './Components/Header/Header';
+import Footer from './Components/Footer/Footer';
+import Home from './Container/Home/Home';
+import Department from './Container/Department/Department';
+import { Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import Doctors from './Container/Doctors/Doctors';
+import About from './Container/About/About';
+import Contact from './Container/Contact/Contact';
+import Appointment from './Container/Appointment/Appointment';
+import Auth from './Container/Auth/Auth';
+import Counter from './Container/Counter/Counter';
+
+
 function App() {
-
-
-
   return (
     <>
-    {/* <Smd /> */}
-    {/* <State /> */}
-    {/* <Country/> */}
-    {/* <StateFun /> */}
-    {/* <City /> */}
-    <Counter />
-    </>    
-    
-  );
+    <Header />
+    <Switch>
+      {/* <Route exact path={"/"} component={Home} />
+      <Route exact path={"/Department"} component={Department} /> */}
+      <Route exact path={"/"} component={Home}/>
+      <Route exact path={"/department"} component={Department} />
+      <Route exact path={"/doctors"} component={Doctors} />
+      <Route exact path={"/about"} component={About} />
+      <Route exact path={"/contact"} component={Contact} />
+      <Route exact path={"/appointment"} component={Appointment} />
+      <Route exact path={"/auth"} component={Auth} />
+     </Switch>
+    <Footer />
+    <Counter />                
+    </>
+  );  
 }
-
+ 
 export default App;
